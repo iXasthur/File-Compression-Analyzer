d4f2c8c6f173f517edf534294843d179e8705068
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-03-18 20:37:19 +0300

    Commit #1

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,18 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Menus,
+  System.ImageList, Vcl.ImgList, Vcl.Buttons;
 
 type
   TForm1 = class(TForm)
+    Label1: TLabel;
+    GridPanel1: TGridPanel;
+    OpenDialog1: TOpenDialog;
+    ImageList1: TImageList;
+    SpeedButton1: TSpeedButton;
+    Label2: TLabel;
+    procedure Label1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,6 +29,15 @@ implementation
 
 {$R *.dfm}
 
-
+procedure TForm1.Label1Click(Sender: TObject);
+begin
+  if Label1.Caption = '(c)Mikhail Kavaleuski ' then
+  begin
+    Label1.Caption := '¡¿Õ! '
+  end else
+      begin
+        Label1.Caption := '(c)Mikhail Kavaleuski '
+      end;
+end;
 
 end.
